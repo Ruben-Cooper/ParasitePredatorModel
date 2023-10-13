b3a823b57cce3d68f2e6942f2b742f274175de42
TimStacey1 <104255715+TimStacey1@users.noreply.github.com>
2023-10-13 17:33:19 +1000

    Comment update

--- Task_3_Tim.m
+++ Task_3_Tim.m
@@ -4,8 +4,8 @@ k4_range = [0 50];
 k5_range = [0 50];
 % the lhs will probably need some adjusting but it seems pretty good
 % the only difference between this and the inbuilt is that gives back
-% floating point values between (0,1) where as the one i made returns
-% integers between the range given and with the amount of bins
+% floating point values between (0,1) i can get the same result by making
+% the ranges [0 1] but having be any range better in the long run
 % shouldn't really be functionally different just getting values from the
 % rands of the inbuilf lhs.
 test = Latin_Hypercube_Sampling_3d_Tim(bins, k3_range, k4_range, k5_range);
