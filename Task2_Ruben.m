8414929f54233783b98b6289b33c59c6515047bd
Ruben <ruben.cooper03@gmail.com>
2023-10-18 15:13:11 +1000

    deleted unnecessary files - added further comments, modified scatterplot, added boundary line

--- Task2_Ruben.m
+++ Task2_Ruben.m
@@ -3,6 +3,7 @@ n = 500;
 samples = lhs2D(n);
 Tol = 0.1;
 
+% Initialise empty matrices to store successful samples for both cases
 successful_samples_i = [];
 successful_samples_ii = [];
 
@@ -16,12 +17,20 @@ for i = 1:n
     end
 end
 
-% Visualization
+% Create Scatter Plot Visualisation
 figure;
 scatter(successful_samples_i(:, 1), successful_samples_i(:, 2), 'r', 'filled');
 hold on;
 scatter(successful_samples_ii(:, 1), successful_samples_ii(:, 2), 'b', 'filled');
+
+% Plotting the boundry line
+x_vals = linspace(0, 50, 1000);  % Generating 1000 points between 0 and 50 for x-axis
+y_vals = 0.5205 * x_vals - 0.3914;  % Using the equation of the line to get corresponding y values
+plot(x_vals, y_vals, 'g', 'LineWidth', 2);  % Plotting the line in green color with a thicker line width
+axis([0 50 0 50]);
+
+% Set labels, title and legend for the plot
 xlabel('k3');
 ylabel('k4');
-title('k3 vs k4');
-legend('0 <= X1(T) <= 0 + Tol', '2 - Tol <= X2(T) <= 2 + Tol', 'Location', 'southoutside');
+title('Impact of k3 and k4 on Parasite-Food Dynamics');
+legend('Case i', 'Case ii', 'Location', 'southoutside');
--- simulate_system.m
+++ simulate_system.m
@@ -1,5 +1,6 @@
 function [X1, X2] = simulate_system(k3, k4, T)
-    % Define the differential equations
+
+    % Define the differential equations, reminder: k1, k2 & k3 are constant
     ode_system = @(t, y) [y(1)*y(2) - 2*y(1); 
                           k3 - k4*y(2) - 3*y(1)];
 
@@ -9,6 +10,7 @@ function [X1, X2] = simulate_system(k3, k4, T)
     % Integrate
     [t, y] = ode45(ode_system, [0 T], y0);
 
+    % Output the final values of X1 & X2
     X1 = y(end, 1);
     X2 = y(end, 2);
 end
