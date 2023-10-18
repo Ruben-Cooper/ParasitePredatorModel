d64af6cafe2e32427ef63f63c536cc4ad56009ef
Ruben <ruben.cooper03@gmail.com>
2023-10-19 09:47:32 +1000

    Changed scatterplot title size and increase number of samples, boundary line also updated

--- Task2_Ruben.m
+++ Task2_Ruben.m
@@ -1,5 +1,5 @@
 % Number of samples
-n = 500;
+n = 5000;
 samples = lhs2D(n);
 Tol = 0.1;
 
@@ -24,13 +24,14 @@ hold on;
 scatter(successful_samples_ii(:, 1), successful_samples_ii(:, 2), 'b', 'filled');
 
 % Plotting the boundry line
-x_vals = linspace(0, 50, 1000);  % Generating 1000 points between 0 and 50 for x-axis
-y_vals = 0.5205 * x_vals - 0.3914;  % Using the equation of the line to get corresponding y values
-plot(x_vals, y_vals, 'g', 'LineWidth', 2);  % Plotting the line in green color with a thicker line width
+x_vals = linspace(0, 50, 1000);   
+y_vals = 0.5367 * x_vals - 0.9190; % Using the equation of the line to get corresponding y values
+plot(x_vals, y_vals, 'g', 'LineWidth', 4);  
 axis([0 50 0 50]);
 
 % Set labels, title and legend for the plot
-xlabel('k3');
-ylabel('k4');
-title('Impact of k3 and k4 on Parasite-Food Dynamics');
-legend('Case i', 'Case ii', 'Location', 'southoutside');
+fontsize = 20;
+xlabel('k3', 'FontSize', fontsize);
+ylabel('k4', 'FontSize', fontsize);
+title('Impact of k3 and k4 on Parasite-Food Dynamics', 'FontSize', fontsize);
+legend(' Case i', ' Case ii', ' Boundary ', 'Location', 'southoutside', 'FontSize', fontsize);
