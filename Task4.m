aefbd0829477dd0b29f0453192c57fd264e9a395
Jakob <Jakob@Jakob-Desktop>
2023-10-19 12:52:20 +1000

    Default values. Very much a work in progress.

--- Task4.m
+++ Task4.m
@@ -1,1 +1,11 @@
 %% Task 4 - Spatial agent-based implementation
+
+%% Scenario 1
+populationDensity = 0.1;
+placementStrategy = "random";
+parasiteMaxAge = 10;
+foodRotChance = 0.01;
+foodAdded = 300;
+gridWidth = 200;
+simulationLength = 400;
+Task4Simulation(parasiteMaxAge, foodRotChance, foodAdded, gridWidth, simulationLength);
